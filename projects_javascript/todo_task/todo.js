2fb94eb1b002381573c343c55ee7b6c0d48bf344
nandakishore <nandakishore695@gmail.com>
2025-09-16 14:47:36 +0530

    todo modal

---
+++ projects_javascript/todo_task/todo.js
@@ -0,0 +1,8 @@
+const todoTitle = document.getElementById("todoTitle");
+const todoDate = document.getElementById("todoDate");
+const todoDescription = document.getElementById("todoDescription");
+const todoCreate = document.getElementById("todoCreate");
+
+todoCreate.addEventListener("click", function(){
+    
+})
