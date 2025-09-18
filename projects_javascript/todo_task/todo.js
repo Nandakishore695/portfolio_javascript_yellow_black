df81854929ddfc464828e35c33340c764b51b66d
nandakishore <nandakishore695@gmail.com>
2025-09-18 13:29:04 +0530

    calculator

--- projects_javascript/todo_task/todo.js
+++ projects_javascript/todo_task/todo.js
@@ -3,16 +3,34 @@ const todoDate = document.getElementById("todoDate");
 const todoDescription = document.getElementById("todoDescription");
 const todoCreate = document.getElementById("todoCreate");
 
+let todoArrayList = [];
+
 todoCreate.addEventListener("click", function () {
-    const titleIs = todoTitle.value
-    const dateIs = todoDate.value
-    const descriptonIs = todoDescription.value
-    const todoHtml = `
-    <div class="feature col-3 mt-0 p-1 ">
-        <div class="border border-black rounded-3 p-2">
-            <h2 class="fs-2 text-body-emphasis">${titleIs}</h2>
-            ${dateIs}
-            <p>${descriptonIs}</p> <a href="#" class="icon-link"
+    const levelWork = document.querySelector('input[name="levelWork"]:checked');
+    if (levelWork) {
+        console.log("Selected value:", levelWork.value);
+    } else {
+        console.log("No option selected");
+    }
+
+    let object = {
+        "id": "1",
+        "titleIs": todoTitle.value,
+        "dateIs": todoDate.value,
+        "priority": levelWork.value,
+        "descriptonIs": todoDescription.value,
+    }
+    todoArrayList.push(object);
+
+    const todoHtml = todoArrayList.map(item => (
+        `
+    <div class="feature col-3 mt-0 p-1" style="height: 2702x;">
+        <div class="border border-black rounded-3 p-2" >
+            <h2 class="fs-2 text-body-emphasis">${item.titleIs}</h2>
+            ${item.dateIs}
+            <p>${item.descriptonIs}</p> 
+            <p>Priority:- ${item.priority === "extreme" ? '<span style="color: red">Extreme</span>' : item.priority === "moderate" ? '<span style="color: blue">Moderate</span>' : item.priority === "low" ? '<span style="color: green">Low</span>' : ""}</p>
+            <a href="#" class="icon-link"
                 data-bs-toggle="modal" data-bs-target="#todoListModal">
                 View
                 <svg class="bi" aria-hidden="true">
@@ -20,5 +38,7 @@ todoCreate.addEventListener("click", function () {
                 </svg> </a>
         </div>
     </div>`
+    ));
+    // const todoHtml = 
     document.getElementById("output").innerHTML = todoHtml
 });
