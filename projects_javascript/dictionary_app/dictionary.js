ec0c3d9275c3459c84d268f573de7a35f005e082
nandakishore <nandakishore695@gmail.com>
2025-09-27 13:11:22 +0530

    dictionary_chg

--- projects_javascript/dictionary_app/dictionary.js
+++ projects_javascript/dictionary_app/dictionary.js
@@ -3,10 +3,8 @@ const wordSearchInput = document.querySelector("#wordSearchInput");
 const result = document.querySelector("#result");
 let isInput = null;
 
-
 wordSearch.addEventListener("submit", function (event) {
     event.preventDefault();
-    debugger
     isInput = wordSearchInput.value.toLowerCase().trim();
     if (!isInput) {
         result.style.color = "red";
@@ -18,15 +16,25 @@ wordSearch.addEventListener("submit", function (event) {
 })
 
 async function dictionaryapi() {
+    debugger
+    try{
     const respone = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${isInput}`);
+    if(!respone.ok){
+        throw new Error(`HTTP error! status: ${respone.status}`);
+    }
+    else{
     const reRespone = await respone.json()
-    console.log(reRespone);
     result.style.color = "black";
     result.innerHTML = `<div >
                 <h2>Word: ${reRespone[0].word}</h2>
                 <p>${reRespone[0].meanings[0].partOfSpeech}</p>
                 <p><strong>Meaning:</strong> ${reRespone[0].meanings[0].definitions[0].definition}</p>
                 <p><strong>Example:</strong> ${reRespone[0].meanings[0].definitions[0].example || reRespone[0].meanings[1].definitions[0].example}</p>
-                <a href="${reRespone[0].sourceUrls[1]}">Read More</a>
+                <a href="${reRespone[0].sourceUrls[0]}" target="_blank" >Read More</a>
             </div>`
+    }
+    }
+    catch(error){
+        result.textContent = error.message;
+    }
 }
